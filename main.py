57d114759921a7a8247508655f0f73a2a49d2470
sonnenbrilln <121475947+sonnenbrilln@users.noreply.github.com>
2024-11-20 16:12:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,75 @@
 
 import time
 
-tambah = 'y'
+tambah = ''
 
-while tambah == 'y':
+while tambah != 'y':
+	
 	mode = input("Pilih Mode [Manual (m) / Cook (c) / Steam (s) / Defrost (d) / Reheat (r)]: ")
 
 	if mode == 'm':
 		temp = input("Masukkan temperatur: ")
 		timer = int(input("Masukkan waktu (dalam detik): "))
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-
 		for x in range(timer, 0, -1):
-			print(x)
-			time.sleep(1)
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 
 	elif mode == 'c':
-		print("Anda memasak selama 100 detik dengan suhu 160 celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		for x in range(120, 0, -1):
-			print(x)
-			time.sleep(1)
-
+		temp = 160
+		timer = 15
+		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
+		for x in range(timer, 0, -1):
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 	elif mode == 's':
-		print("Anda memasak selama 60 detik dengan suhu 100 celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		for x in range(60, 0, -1):
-			print(x)
-			time.sleep(1)
+		temp = 100
+		timer = 15
+		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
+		for x in range(timer, 0, -1):
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 
 	elif mode == 'd':
-		print("Anda memasak selama 100 detik dengan suhu 60 celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		for x in range(100, 0, -1):
-			print(x)
-			time.sleep(1)
+		temp = 40
+		timer = 10
+		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
+		for x in range(timer, 0, -1):
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 
 	elif mode == 'r':
-		print("Anda memasak selama 40 detik dengan suhu 40 celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		for x in range(40, 0, -1):
-			print(x)
-			time.sleep(1)
+		temp = 60
+		timer = 5
+		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
+		for x in range(timer, 0, -1):
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 
-	print("Makanan sudah matang!")
+	else: # manual
+		temp = input("Masukkan temperatur: ")
+		timer = int(input("Masukkan waktu (dalam detik): "))
+		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
+		for x in range(timer, 0, -1):
+                        seconds = x % 60
+                        minutes = int(x / 60) % 60
+                        hours = int(x / 3600)
+                        print(f"{hours:02}:{minutes:02}:{seconds:02}")
+                        time.sleep(1)
 
-	tambah = input("Makanan sudah matang! Apakah anda mau menambah waktu? [Ya (y)])")
+	tambah = input("Makanan sudah matang! Matikan microwave? [Ya (y) / Tidak (tombol lain)]: ")
