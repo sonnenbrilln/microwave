148af52abba22f8c43c61496226f370952aec99f
sonnenbrilln <121475947+sonnenbrilln@users.noreply.github.com>
2024-11-21 14:01:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ Mini Project mata kuliah Pengenalan Komputasi ITB tahun ajaran 2024/2025
 
 Kelompok 10 K-05
 
-Nama				NIM
-Nafilah Adinata 		16024364
-Nadha Aulia Ramadhani		16024380
-Khalifa Prasetya Al Kautsar	16024394
-Fathir Hasya Adrian		16024410
-Talitha Siti Salsabila		16024426
-Muhammad Hilmiy			16024455 
-
+Nama							NIM
+Nafilah Adinata 				16024364
+Nadha Aulia Ramadhani			16024380
+Khalifa Prasetya Al Kautsar		16024394
+Fathir Hasya Adrian				16024410
+Talitha Siti Salsabila			16024426
+Muhammad Hilmiy					16024455 
 
 
 Kamus
@@ -23,6 +22,15 @@ timer	: int, waktu yang diinput user
 '''
 
 import time
+
+# Subprogram hitung mundur
+def hitung_mundur(timer):
+	for x in range(timer, 0, -1):
+		seconds = x % 60
+		minutes = int(x / 60) % 60
+		hours = int(x / 3600)
+		print(f"{hours:02}:{minutes:02}:{seconds:02}")
+		time.sleep(1)
 
 selesai = ''
 
@@ -36,84 +44,42 @@ while selesai != 'y':
 		temp = input("Masukkan temperatur: ")
 		timer = int(input("Masukkan waktu (dalam detik): "))
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 
 	# Apabila user memilih mode cook, preset yang digunakan adalah temp = 160 ; timer = 15
 	elif mode == 'c':
 		temp = 160
 		timer = 15
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 
 	# Apabila user memilih mode steam, preset yang digunakan adalah temp = 100 ; timer = 15
 	elif mode == 's':
 		temp = 100
 		timer = 15
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 
 	# Apabila user memilih mode defrost, preset yang digunakan adalah temp = 40 ; timer = 10
 	elif mode == 'd':
 		temp = 40
 		timer = 10
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 
 	# Apabila user memilih mode reheat, preset yang digunakan adalah temp = 60 ; timer = 5
 	elif mode == 'r':
 		temp = 60
 		timer = 5
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 	
 	# Apabila user menekan tombol lain, akan dipilih mode manual
 	else : # manual
 		temp = input("Masukkan temperatur: ")
 		timer = int(input("Masukkan waktu (dalam detik): "))
 		print("Anda memasak selama ", timer, " detik dengan suhu ", temp, " celcius. Tekan Ctrl + C untuk mematikan microwave.")
-		
-		# Hitung mundur
-		for x in range(timer, 0, -1):
-			seconds = x % 60
-			minutes = int(x / 60) % 60
-			hours = int(x / 3600)
-			print(f"{hours:02}:{minutes:02}:{seconds:02}")
-			time.sleep(1)
+		hitung_mundur(timer)
 	
 	# Menanyakan apakah user masih mau memasak atau mematikan microwave, jika masih mau memasak, program tidak akan keluar dari loop sehingga mengulang prosesnya.
 	selesai = input("Makanan sudah matang! Matikan microwave? [Ya (y) / Tidak (tombol lain)]: ")
